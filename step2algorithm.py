8b825a7ccb44a56b67ef027e6b8fd4a64329f00b
bhavyad09 <157430117+bhavyad09@users.noreply.github.com>
2024-09-13 13:27:53 -0700

    Update step2algorithm.py

--- step2algorithm.py
+++ step2algorithm.py
@@ -1,7 +1,6 @@
 import turtle as trtl
 
 t = trtl.Turtle()
-
 screen = trtl.Screen()
 screen.setup(width=800, height=600) 
 
@@ -10,22 +9,16 @@ def drawSq(t, size):
         t.forward(size)
         t.right(90)
 
-def putObj(numObj):
-      
+def putObj(numObj):    
     spacing = (800 - numObj * 50) / (numObj + 1)
-    
     start_x = -(800 / 2) + spacing + (50 / 2)
     y=0
-
     t.penup()
-    
     for j in range(numObj):
         t.goto(start_x + j * (50 + spacing), y)
         t.pendown()
         drawSq(t, 50)
         t.penup()
-
-    t.hideturtle()
 
 userInput = int(input("How many objects do you want?"))
 putObj(userInput)
